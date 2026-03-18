2a34875d35ded7e8423407b9a2d42f6fbdd735a9
svnilr <sebastiananilr@gmail.com>
2026-03-18 16:45:32 -0300

    Improve: Make start.sh more robust with directory detection

--- nike-bot-pro/start.sh
+++ nike-bot-pro/start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-cd auth_server
-uvicorn main:app --host 0.0.0.0 --port $PORT
+set -e  # Exit on error
+
+# Find auth_server directory and run from there
+if [ -d "nike-bot-pro/auth_server" ]; then
+    cd nike-bot-pro/auth_server
+elif [ -d "auth_server" ]; then
+    cd auth_server
+fi
+
+# Run uvicorn
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8080}
+
